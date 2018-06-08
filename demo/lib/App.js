7879f5d5ccce4a6d27b6b35323e1f3f3d0102771
kaan.alkim <g.kaanalkim@gmail.com>
2018-06-08 22:17:31 +0300

    pub/sub pattern implementation for state management

--- demo/lib/App.js
+++ demo/lib/App.js
@@ -1,3 +1,5 @@
+var eventbus = null;
+
 $(document).ready(function () {
     new Button().usage();
 
@@ -10,4 +12,21 @@ $(document).ready(function () {
     new DataViewer().usage();
 
     new Selection().usage();
+
+    pubsub();
 });
+
+var sub1;
+
+function pubsub() {
+    eventbus = new Eventbus();
+
+    eventbus.publish('event_1', {
+        foo: 'bar tar'
+    });
+
+    //TODO a subscriber should not subscribe to the same channel more than once, subscriber should define an ID during subscription
+    eventbus.subscribe('event_1', function (state) {
+        console.log('event_1 -> listener 1 -> state.foo = ' + state.foo + '}');
+    });
+}
