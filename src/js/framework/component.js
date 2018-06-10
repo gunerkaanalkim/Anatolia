b09782f5907012e6c5c56cbe68738b80c18f15dc
kaan.alkim <g.kaanalkim@gmail.com>
2018-06-10 20:17:48 +0300

    component api

--- src/js/framework/component.js
+++ src/js/framework/component.js
@@ -3,22 +3,39 @@ var Component = (function () {
         this._subscriber = null;
         this._name = name;
         this._options = options;
+        this._options = options;
 
+        this._container = this._options.container;
         this._template = this._options.template;
         this._eventbus = this._options.eventbus;
 
-        this._render(this._name, this._template, this._eventbus);
+        this._render(this._name, this._template, this._container);
     }
 
-    Component.prototype._render = function (componentName, template, eventbus) {
+    Component.prototype._render = function (componentName, rawTemplate, container) {
+
         this._subscriber = new Subscriber('event1', function (state) {
-            var componentContainer = document.createElement(componentName);
-
-
             console.log(state);
+
+            var template = rawTemplate;
+
+            var regex = /{{\s*([^}]+)\s*}}/g;
+            var item;
+
+            while (item = regex.exec(template)) {
+                var replacedItem = item[0].trim();
+                var searchedItem = item[1].trim();
+
+                if (state.hasOwnProperty(searchedItem)) {
+                    template = template.replace(replacedItem, state[searchedItem]);
+                }
+            }
+
+
+            document.querySelector(container).innerHTML = template;
         });
 
-        eventbus.subscriber().register(this._subscriber);
+        this._eventbus.subscriber().register(this._subscriber);
     };
 
     Component.prototype.fire = function () {
