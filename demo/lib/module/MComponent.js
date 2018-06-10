aeacdda2eb18d00c7f5482faab5e79c6ba6cf9d3
kaan.alkim <g.kaanalkim@gmail.com>
2018-06-10 21:26:03 +0300

    component api test

--- demo/lib/module/MComponent.js
+++ demo/lib/module/MComponent.js
@@ -15,25 +15,17 @@ var Comp = (function () {
             text_1: new LoremIpsum().generate(5),
             text_2: new LoremIpsum().generate(5),
             text_3: new LoremIpsum().generate(5),
-            style: "text-align:center; padding:50px; background-color: #c1c1c1; color: blue; "
+            text_4: new LoremIpsum().generate(5),
+            text_5: new LoremIpsum().generate(5),
+            style: "font-size: 20px; text-align:center; padding:50px; background-color: " + '#' + Math.floor(Math.random() * 16777215).toString(16) + "; color: " + '#' + Math.floor(Math.random() * 16777215).toString(16) + "; "
         });
         eventbus.publisher().register(pub);
-
-        setInterval(function (args) {
-            pub = new Publisher('event1', {
-                text_1: new LoremIpsum().generate(5),
-                text_2: new LoremIpsum().generate(5),
-                text_3: new LoremIpsum().generate(5),
-                style: "text-align:center; padding:50px; background-color: " + '#' + Math.floor(Math.random() * 16777215).toString(16) + "; color: " + '#' + Math.floor(Math.random() * 16777215).toString(16) + "; "
-            });
-            eventbus.publisher().register(pub);
-        }, 2000);
 
         component = new Component('myComponent', {
             container: '#component',
             eventbus: eventbus,
             event: 'event1',
-            template: "<div style='{{style}}'><p>{{text_1}}</p> <p>{{text_1}}</p> <p>{{text_1}}</p> </div>"
+            template: "<div style='{{style}}'> <p>{{text_1}}</p> <p>{{text_2}}</p> <p>{{text_3}}</p> <p>{{text_4}}</p> <p>{{text_5}}</p> </div>"
         });
     };
 
