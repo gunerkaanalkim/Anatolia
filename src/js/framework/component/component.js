166400057d4df6984da3344eabe3e6437049c397
kaan.alkim <g.kaanalkim@gmail.com>
2018-06-13 19:59:23 +0300

    code refactoring

--- src/js/framework/component/component.js
+++ src/js/framework/component/component.js
@@ -1,86 +1,85 @@
-var Component = (function () {
-    function Component(name, options) {
-        this._options = options;
-        this._name = name;
+'use strict';
 
-        this._initialize(options);
+function Component(name, options) {
+    this._options = options;
+    this._name = name;
+
+    this._initialize(options);
+}
+
+Component.prototype._initialize = function () {
+    this._subscriber = null;
+    this._template = this._options.template;
+    this._event = this._options.event;
+    this._methods = this._options.methods;
+};
+
+Component.prototype._render = function () {
+    var context = this;
+    if (!this._globalSetting.components.hasOwnProperty(this._name)) {
+        this._globalSetting.components[this._name] = {};
+        this._globalSetting.components[this._name]['methods'] = {};
     }
 
-    Component.prototype._initialize = function () {
-        this._subscriber = null;
-        this._template = this._options.template;
-        this._methods = this._options.methods;
-    };
+    this._subscriber = new Subscriber(this._event, function (state) {
+        var component = document.createElement(context._name);
 
-    Component.prototype._render = function () {
-        var context = this;
-        // window.fon.components[this._name] = {};
-        if (!this._globalSetting.components.hasOwnProperty(this._name)) {
-            this._globalSetting.components[this._name] = {};
-            this._globalSetting.components[this._name]['methods'] = {};
+        var template = context._template;
+
+        var expressionRegex = /{{\s*([^}]+)\s*}}/g;
+        var expression;
+
+        while (expression = expressionRegex.exec(template)) {
+            var replacedExpression = expression[0].trim();
+            var searchedExpression = expression[1].trim();
+
+            if (state.hasOwnProperty(searchedExpression)) {
+                template = template.replace(replacedExpression, state[searchedExpression]);
+            }
         }
 
-        this._subscriber = new Subscriber('event1', function (state) {
-            var component = document.createElement(context._name);
+        var methodsRegex = /f-on:(\S+)\s*=\s*([']|["])\s*([\W\w]*?)\s*\2/g;
+        var method;
 
-            var template = context._template;
+        while (method = methodsRegex.exec(template)) {
+            var replacedMethod = method[0].trim();
+            var searchedMethod = method[1].trim();
+            var event = method[3].trim();
+            var eventKey = method[3].trim().split('(')[0];
 
-            var expressionRegex = /{{\s*([^}]+)\s*}}/g;
-            var expression;
+            //Setting event in global anatolia object
+            context._globalSetting.components[context._name]['methods'][eventKey] = context._methods[eventKey];
 
-            while (expression = expressionRegex.exec(template)) {
-                var replacedExpression = expression[0].trim();
-                var searchedExpression = expression[1].trim();
 
-                if (state.hasOwnProperty(searchedExpression)) {
-                    template = template.replace(replacedExpression, state[searchedExpression]);
-                }
+            if (context._methods.hasOwnProperty(method[3].trim().split('(')[0])) {
+                template = template.replace(replacedMethod, 'on' + searchedMethod + '=\'window._anatolia.components.' + context._name + '.methods.' + event + '\'');
             }
+        }
 
-            var methodsRegex = /f-on:(\S+)\s*=\s*([']|["])\s*([\W\w]*?)\s*\2/g;
+        if (context._container !== undefined) {
+            document.querySelector(context._container).innerHTML = template;
+        } else {
+            throw 'Undefined container for component : ' + context._name;
+        }
+    });
 
-            while (method = methodsRegex.exec(template)) {
-                var replacedMethod = method[0].trim();
-                var searchedMethod = method[1].trim();
-                var event = method[3].trim();
-                var eventKey = method[3].trim().split('(')[0];
+    console.log(this._subscriber.event());
 
-                //Setting event in global anatolia object
-                context._globalSetting.components[context._name]['methods'][eventKey] = context._methods[eventKey];
+    this._eventbus.subscriber().register(this._subscriber);
+};
 
+Component.prototype.fire = function () {
+    this._eventbus.subscriber().fire(this._subscriber);
+};
 
-                if (context._methods.hasOwnProperty(method[3].trim().split('(')[0])) {
-                    template = template.replace(replacedMethod, 'on' + searchedMethod + '=\'window._anatolia.components.' + context._name + '.methods.' + event + '\'');
-                }
+Component.prototype.setContainer = function (componentContainer) {
+    this._container = componentContainer;
+};
 
-                // window.fon.components[context._name][eventKey] = context._methods[eventKey];
-            }
+Component.prototype.setEventbus = function (eventbus) {
+    this._eventbus = eventbus;
+};
 
-            if (context._container !== undefined) {
-                document.querySelector(context._container).innerHTML = template;
-            } else {
-                throw 'Undefined container for component : ' + context._name;
-            }
-        });
-
-        this._eventbus.subscriber().register(this._subscriber);
-    };
-
-    Component.prototype.fire = function () {
-        this._eventbus.subscriber().fire(this._subscriber);
-    };
-
-    Component.prototype.setContainer = function (componentContainer) {
-        this._container = componentContainer;
-    };
-
-    Component.prototype.setEventbus = function (eventbus) {
-        this._eventbus = eventbus;
-    };
-
-    Component.prototype.setGlobalSetting = function (anatoliaGlobalSetting) {
-        this._globalSetting = anatoliaGlobalSetting;
-    };
-
-    return Component;
-})();
+Component.prototype.setGlobalSetting = function (anatoliaGlobalSetting) {
+    this._globalSetting = anatoliaGlobalSetting;
+};
