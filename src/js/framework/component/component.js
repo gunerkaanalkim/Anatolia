bdbe2dc4245076bab6a62b2ed77f4056527af22f
kaan.alkim <g.kaanalkim@gmail.com>
2018-06-13 19:30:26 +0300

    setting events

--- src/js/framework/component/component.js
+++ src/js/framework/component/component.js
@@ -1,16 +1,24 @@
 var Component = (function () {
     function Component(name, options) {
-        this._subscriber = null;
-        this._name = name;
         this._options = options;
+        this._name = name;
 
+        this._initialize(options);
+    }
+
+    Component.prototype._initialize = function () {
+        this._subscriber = null;
         this._template = this._options.template;
         this._methods = this._options.methods;
-    }
+    };
 
     Component.prototype._render = function () {
         var context = this;
-        window.fon.components[this._name] = {};
+        // window.fon.components[this._name] = {};
+        if (!this._globalSetting.components.hasOwnProperty(this._name)) {
+            this._globalSetting.components[this._name] = {};
+            this._globalSetting.components[this._name]['methods'] = {};
+        }
 
         this._subscriber = new Subscriber('event1', function (state) {
             var component = document.createElement(context._name);
@@ -37,13 +45,16 @@ var Component = (function () {
                 var event = method[3].trim();
                 var eventKey = method[3].trim().split('(')[0];
 
+                //Setting event in global anatolia object
+                context._globalSetting.components[context._name]['methods'][eventKey] = context._methods[eventKey];
+
+
                 if (context._methods.hasOwnProperty(method[3].trim().split('(')[0])) {
-                    template = template.replace(replacedMethod, 'on' + searchedMethod + '=\'window.fon.components.' + context._name + '.' + event + '\'');
+                    template = template.replace(replacedMethod, 'on' + searchedMethod + '=\'window._anatolia.components.' + context._name + '.methods.' + event + '\'');
                 }
 
-                window.fon.components[context._name][eventKey] = context._methods[eventKey];
+                // window.fon.components[context._name][eventKey] = context._methods[eventKey];
             }
-
 
             if (context._container !== undefined) {
                 document.querySelector(context._container).innerHTML = template;
@@ -65,6 +76,10 @@ var Component = (function () {
 
     Component.prototype.setEventbus = function (eventbus) {
         this._eventbus = eventbus;
+    };
+
+    Component.prototype.setGlobalSetting = function (anatoliaGlobalSetting) {
+        this._globalSetting = anatoliaGlobalSetting;
     };
 
     return Component;
