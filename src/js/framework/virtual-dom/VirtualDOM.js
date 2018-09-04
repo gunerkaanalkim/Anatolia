01157d2da24cf07a7bc74b69ff37bbefddb173f4
kaan.alkim <g.kaanalkim@gmail.com>
2018-09-04 19:56:29 +0300

    node comparison and changing list

--- src/js/framework/virtual-dom/VirtualDOM.js
+++ src/js/framework/virtual-dom/VirtualDOM.js
@@ -59,19 +59,28 @@ VirtualDOM.toHTML = function (vDOM) {
 
 VirtualDOM.compare = function (originalVDOM, dirtyVDOM) {
     if (originalVDOM) {
-        console.log(originalVDOM);
-        console.log(dirtyVDOM);
-        // node tipleri ve node adları aynı mı ?
-        if (VirtualNode.isEqualNode(originalVDOM, dirtyVDOM)) {
-            // element create, update, delete
-            if (VirtualNode.hasChild(dirtyVDOM)) {
-                VirtualNode.childNodes.forEach(function (childNode, index) {
+        var nodeComparison = VirtualNode.nodeComparison(originalVDOM, dirtyVDOM);
 
-                });
+        if (!nodeComparison.hasChanges()) {
+            // element create, update, delete
+            if (VirtualNode.hasChild(dirtyVDOM) && VirtualNode.hasChild(originalVDOM)) {
+                var dirtyVDOMChildNodes = VirtualNode.childNodes(dirtyVDOM);
+                var originalVDOMChildNodes = VirtualNode.childNodes(originalVDOM);
+
+                for (var i = 0; i < dirtyVDOMChildNodes.length; i++) {
+                    var dirtyVDOMChildNode = dirtyVDOMChildNodes[i];
+                    var originalVDOMChildNode = originalVDOMChildNodes[i];
+
+                    VirtualDOM.compare(originalVDOMChildNode, dirtyVDOMChildNode);
+                }
+            } else {
+                // TODO : append child node to DOM
+                console.log("append child node to DOM");
             }
-            debugger;
         } else {
-            // re-render component
+            // TODO : re-render component
+            console.log("re-render component");
+            nodeComparison.getChanges();
         }
     }
 };
@@ -82,27 +91,66 @@ VirtualDOM.compare = function (originalVDOM, dirtyVDOM) {
 function VirtualNode() {
 }
 
-VirtualNode.isEqualNode = function isEqual(originalVDOM, dirtyVDOM) {
-    if (!VirtualNode.isEqual(dirtyVDOM.nodeType, originalVDOM.nodeType) && !VirtualNode.isEqual(dirtyVDOM.nodeName, originalVDOM.nodeName)) {
-        return false;
+VirtualNode.nodeComparison = function isEqual(originalVDOM, dirtyVDOM) {
+    var changing;
+
+    var changeList = [];
+
+    /**
+     * Node Type Comparison
+     * **/
+    if (!VirtualNode.isEqual(dirtyVDOM.nodeType, originalVDOM.nodeType)) {
+        changing = VirtualNode.setCause(originalVDOM, "nodeType", originalVDOM.nodeType, dirtyVDOM.nodeType);
+
+        changeList.push(changing);
     }
 
+    /**
+     * Node Name Comparison
+     * **/
+    if (!VirtualNode.isEqual(dirtyVDOM.nodeName, originalVDOM.nodeName)) {
+        changing = VirtualNode.setCause(originalVDOM, "nodeName", originalVDOM.nodeName, dirtyVDOM.nodeName);
+
+        changeList.push(changing);
+    }
+
+    /**
+     * Node value Comparison
+     * **/
     if (VirtualNode.isEqual(dirtyVDOM.nodeType, 3)) {
         if (!VirtualNode.isEqual(dirtyVDOM.nodeValue, originalVDOM.nodeValue)) {
-            return false;
+            changing = VirtualNode.setCause(originalVDOM, "nodeValue", originalVDOM.nodeValue, dirtyVDOM.nodeValue);
+
+            changeList.push(changing);
         }
     }
 
+    /**
+     * Attributes Comparison
+     * **/
     var dirtyVDOMAttributes = dirtyVDOM.attributes;
     var originalVDOMAttributes = originalVDOM.attributes;
 
     for (var attr in dirtyVDOMAttributes) {
         if (!originalVDOMAttributes.hasOwnProperty(attr) && !VirtualNode.isEqual(originalVDOMAttributes[attr], dirtyVDOMAttributes[attr])) {
-            return false;
+            changing = VirtualNode.setCause(originalVDOM, "attributes", originalVDOMAttributes, dirtyVDOMAttributes);
+
+            changeList.push(changing);
         }
     }
 
-    return true;
+    function hasChanges() {
+        return changeList.length > 0;
+    }
+
+    function getChanges() {
+        return changeList;
+    }
+
+    return {
+        hasChanges: hasChanges,
+        getChanges: getChanges
+    };
 };
 
 VirtualNode.hasChild = function (vNode) {
@@ -115,4 +163,15 @@ VirtualNode.childNodes = function (vNode) {
 
 VirtualNode.isEqual = function (val1, val2) {
     return val1 === val2;
+};
+
+VirtualNode.setCause = function (originalVNode, cause, from, to) {
+    return {
+        vNode: originalVNode,
+        changing: {
+            cause: cause,
+            from: from,
+            to: to
+        }
+    };
 };
