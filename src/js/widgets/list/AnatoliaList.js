f7c3c91930a7955b41943f1f70907ef59e6d2ed8
kaan.alkim <g.kaanalkim@gmail.com>
2018-09-05 20:34:06 +0300

    new list component

---
+++ src/js/widgets/list/AnatoliaList.js
@@ -0,0 +1,30 @@
+var AnatoliaList = {
+    render: function (state) {
+        var cc = Component.createElement;
+
+        var container = cc("div");
+        var firstRow = cc("div", {class: "row clearfix"});
+
+        var ul = cc("ul", {class: "list-group"});
+
+        state.list.forEach(function (listItem) {
+            var li = cc("li", {class: "list-group-item", text: listItem.text});
+
+            ul.append(li);
+        });
+
+        firstRow.append(ul);
+
+        var secondRow = cc("div", {class: "row clearfix", style: "margin-top: 30px;"});
+        var pushButton = cc("a", {class: "btn btn-success", text: "PUSH", id: "pushButton"});
+        var popButton = cc("a", {class: "btn btn-danger", text: "POP", id: "popButton"});
+
+        secondRow.append(pushButton);
+        secondRow.append(popButton);
+
+        container.append(secondRow);
+        container.append(firstRow);
+
+        return container;
+    }
+};
