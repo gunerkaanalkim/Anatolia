a2f82751bec4932086c8ba7af0950c3e089ae592
g.kaanalkim@gmail.com <g.kaanalkimqgmail.com>
2018-09-16 01:20:42 +0300

    refactor and util impr

--- src/js/framework/util/Util.js
+++ src/js/framework/util/Util.js
@@ -2,24 +2,55 @@ function Util() {
 
 }
 
-Util.prototype.constructor = Util;
-
-Util.flattenObject = function (obj) {
-    var toReturn = {};
-
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-
-        if ((typeof obj[i]) === 'object') {
-            var flatObject = Util.flattenObject(obj[i]);
-            for (var x in flatObject) {
-                if (!flatObject.hasOwnProperty(x)) continue;
-
-                toReturn[i + '.' + x] = flatObject[x];
-            }
-        } else {
-            toReturn[i] = obj[i];
+Util.is = {
+    defined: function (value) {
+        return typeof value !== "undefined"
+    },
+    null: function (value) {
+        return value === null;
+    },
+    equal: function (val1, val2) {
+        return val1 === val2;
+    },
+    function: function (val) {
+        return typeof val === "function";
+    },
+    object: function (val) {
+        return typeof val === "object";
+    },
+    string: function (val) {
+        return typeof val === "string";
+    },
+    number: function (val) {
+        return typeof val === "number";
+    },
+    date: function (val) {
+        return typeof val.getMonth === "function";
+    },
+    not: {
+        defined: function (value) {
+            return !Util.is.defined(value);
+        },
+        null: function (value) {
+            return !Util.is.null(value);
+        },
+        equal: function (val1, val2) {
+            return !Util.is.equal(val1, val2);
+        },
+        function: function (val) {
+            return !Util.is.function(val);
+        },
+        object: function (val) {
+            return !Util.is.object(val);
+        },
+        string: function (val) {
+            return !Util.is.string(val);
+        },
+        number: function (val) {
+            return !Util.is.number(val);
+        },
+        date: function (val) {
+            return !Util.is.date(val);
         }
     }
-    return toReturn;
 };
