cbd0c768622fcd1148a357b7f0785063b7a6ec95
g.kaanalkim@gmail.com <g.kaanalkimqgmail.com>
2018-09-16 14:12:41 +0300

    re-create with component's static render method

--- src/js/widgets/button/AnatoliaButton.js
+++ src/js/widgets/button/AnatoliaButton.js
@@ -1,62 +1,65 @@
 /**
- * Anatolia Button 's render method. Returns <button> instance.
+ * Anatolia Button Component.
+ * Returns <button> instance.
  *
- * @summary Anatolia Button
+ * @summary creates a <button> element
  * @author Güner Kaan ALKIM <g.kaanalkim@gmail.com>
- * @version 0.1.0
+ * @version 1.0.1
  *
- * @param {object}  attributes  - Attributes are define <button> element 's attribute. Ex; class, id or custom attribute
- * @param {object}  fontIcon    - Defines font icon. Ex: "fa fa-home"
- * @param {object}  text        - Defines button text. Ex: "My Button"
+ * @param {object}  state               - Attributes are define <button> element 's attribute. Ex; class, id or custom attribute
+ * @param {object}  state.attributes    - Attributes are define <button> element 's attribute. Ex; class, id or custom attribute
+ * @param {string}  state.fontIcon      - Defines font icon. Ex: "fa fa-home"
+ * @param {string}  state.text          - Defines button text. Ex: "My Button"
  * **/
 
-var AnatoliaButton = {
-    render: function (state) {
-        /**
-         * Toolkit declarations
-         * **/
-        var create = Component.createElement;
-        var is = Util.is;
+'use strict';
 
-        /**
-         * Attributes declarations
-         * **/
-        var attributes = state.attributes;
-        var buttonIcon = state.fontIcon;
-        var buttonText = state.text;
+var AnatoliaButton = Component.setRenderMethod(function (state) {
+    //  Toolkit declarations
+    var create = Component.createElement;
+    var is = Util.is;
 
-        /**
-         * DOM Element declarations
-         * **/
-        var button = create("button", {type: "button"});
+    //  Attributes declarations
+    var attributes = state.attributes;
+    var buttonIcon = state.fontIcon;
+    var buttonText = state.text;
 
-        /**
-         * Attribute assignment
-         * **/
-        if (is.defined(attributes) && is.not.null(attributes) && is.object(attributes)) {
-            for (var name in attributes) {
-                button.setAttribute(name, attributes[name])
-            }
+    //  DOM Element declarations
+    var button = create("button", {type: "button"});
+
+    //  Attribute assignment
+    if (is.defined(attributes) && is.not.null(attributes) && is.object(attributes)) {
+        for (var name in attributes) {
+            button.setAttribute(name, attributes[name])
         }
-
-        /**
-         * Button Icon
-         * **/
-        if (is.defined(buttonIcon) && is.not.null(buttonIcon) && is.string(buttonIcon)) {
-            var icon = create("i", {class: buttonIcon});
-
-            button.append(icon);
-        }
-
-        /**
-         * Button Text
-         * **/
-        if (is.defined(buttonIcon) && is.not.null(buttonIcon) && is.string(buttonIcon)) {
-            var text = create("span", {text: buttonText});
-
-            button.append(text);
-        }
-
-        return button;
     }
-};
+
+    //  Button Icon
+    if (is.defined(buttonIcon) && is.not.null(buttonIcon) && is.string(buttonIcon)) {
+        var icon = create("i", {class: buttonIcon});
+
+        button.append(icon);
+    }
+
+    //  Button Text
+    if (is.defined(buttonIcon) && is.not.null(buttonIcon) && is.string(buttonIcon)) {
+        var text = create("span", {text: buttonText});
+
+        button.append(text);
+    }
+
+    return button;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
