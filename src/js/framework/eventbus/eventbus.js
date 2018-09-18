4cf4adde4966ab7880909499f0606e1133a38c5b
gunerkaanalkim <g.kaanalkim@gmail.com>
2018-09-19 00:05:57 +0300

    jsdoc 1.0.0

--- src/js/framework/eventbus/eventbus.js
+++ src/js/framework/eventbus/eventbus.js
@@ -1,39 +1,89 @@
 'use strict';
 
+/**
+ * Anatolia Eventbus
+ * Returns observable object
+ *
+ * @summary Observable event-bus object
+ * @author Güner Kaan ALKIM <g.kaanalkim@gmail.com>
+ *
+ * @constructor
+ * @version 1.0.0
+ * @function   Eventbus
+ * @return     Eventbus instance
+ * **/
 function Eventbus() {
     this._eventbus = {};
     this._mute = false;
 }
 
 /**
- * Eventbus Operation
+ *  @summary    Listen to Evenbus object
+ *
+ *  @public
+ *  @function   listen
+ *
+ *  @return     eventbus instance
+ *
  * **/
 Eventbus.prototype.listen = function () {
     return this._eventbus;
 };
 
+/**
+ *  @summary    Stops all events
+ *
+ *  @public
+ *  @function   mute
+ *
+ * **/
 Eventbus.prototype.mute = function () {
     if (!this._mute) {
         this._mute = true;
     }
 };
 
+/**
+ *  @summary    Starts all events
+ *
+ *  @public
+ *  @function   unmuted
+ *
+ * **/
 Eventbus.prototype.unmuted = function () {
     if (this._mute) {
         this._mute = false;
     }
 };
 
+/**
+ *  @summary    Removes all publisher and their subscriber
+ *
+ *  @public
+ *  @function   clean
+ *
+ * **/
 Eventbus.prototype.clean = function () {
     this._eventbus = {};
 };
 
+/**
+ *  @summary    Gets all subscribable events
+ *
+ *  @public
+ *  @function   getSubscriptionList
+ *
+ * **/
 Eventbus.prototype.getSubscriptionList = function () {
     return Object.keys(this._eventbus);
 };
 
 /**
- * Publisher Operation
+ *  @function           publisher
+ *
+ *  @public
+ *  @return {object}    Return value contains two function; register and fire.
+ *
  * **/
 Eventbus.prototype.publisher = function () {
     return {
@@ -42,6 +92,13 @@ Eventbus.prototype.publisher = function () {
     }
 };
 
+/**
+ *  @summary    Used to register publishers to eventbus
+ *
+ *
+ *  @function   _registerPublisher
+ *
+ * **/
 Eventbus.prototype._registerPublisher = function () {
     for (var i in arguments) {
         var publisher = arguments[i];
@@ -50,6 +107,15 @@ Eventbus.prototype._registerPublisher = function () {
     }
 };
 
+/**
+ *  @summary    Used to fill a publisher to eventbus
+ *
+ *
+ *  @function   _fillPublishers
+ *
+ *  @param      {object}    Publisher
+ *
+ * **/
 Eventbus.prototype._fillPublishers = function (publisher) {
     var event = publisher.event();
     var state = publisher.state();
@@ -68,6 +134,15 @@ Eventbus.prototype._fillPublishers = function (publisher) {
     this._firePublisher(publisher);
 };
 
+/**
+ *  @summary    Used to trigger publisher's subscribers
+ *
+ *
+ *  @function   _firePublisher
+ *
+ *  @param  {object}    Publisher
+ *
+ * **/
 Eventbus.prototype._firePublisher = function (publisher) {
     var context = this;
 
@@ -108,7 +183,11 @@ Eventbus.prototype._firePublisher = function (publisher) {
 };
 
 /**
- * Subscriber Operation
+ *  @function           subscriber
+ *
+ *  @public
+ *  @return {object}    Return value contains two function; register and fire.
+ *
  * **/
 Eventbus.prototype.subscriber = function () {
     return {
@@ -117,6 +196,13 @@ Eventbus.prototype.subscriber = function () {
     }
 };
 
+/**
+ *  @summary    Used to register subscribers to publisher
+ *
+ *
+ *  @function   _registerSubscriber
+ *
+ * **/
 Eventbus.prototype._registerSubscriber = function () {
     for (var i in arguments) {
         var subscriber = arguments[i];
@@ -125,6 +211,15 @@ Eventbus.prototype._registerSubscriber = function () {
     }
 };
 
+/**
+ *  @summary    Used to fill a subscriber to publisher
+ *
+ *
+ *  @function   _fillSubscribers
+ *
+ *  @param      {object}    Subscriber
+ *
+ * **/
 Eventbus.prototype._fillSubscribers = function (subscriber) {
     var context = this;
     var states = {};
@@ -164,6 +259,15 @@ Eventbus.prototype._fillSubscribers = function (subscriber) {
     }
 };
 
+/**
+ *  @summary    Used to trigger a subscribers
+ *
+ *
+ *  @function   _firePublisher
+ *
+ *  @param  {object}    Publisher
+ *
+ * **/
 Eventbus.prototype._fireSubscriber = function (subscriber) {
     if (!this._mute) {
         var context = this;
@@ -199,12 +303,48 @@ Eventbus.prototype._fireSubscriber = function (subscriber) {
 };
 
 /**
- * Publisher
+ *  @summary    Eventbus contains this event returns true
+ *
+ *
+ *  @function   _isEvent
+ *
+ *  @param  {boolean}   if eventbus contains this event
+ *
+ * **/
+Eventbus.prototype._isEvent = function (eventName) {
+    if (this._eventbus.hasOwnProperty(eventName)) {
+        return true;
+    }
+
+    throw "Anatolia Error : Unknown event!"
+};
+
+/**
+ *  @constructor
+ *
+ *  @function   Publisher
+ *
+ *  @param  {object}    options
+ *  @return {object}    Publisher instance
+ *
  * **/
 function Publisher(options) {
     this._init(options);
 }
 
+/**
+ *  @summary    private constructor's member
+ *
+ *
+ *  @function   _init
+ *
+ *  @param      {object}    options
+ *  @param      {string}    options.event               -   Knowledge where to publish state object
+ *  @param      {string}    options.state               -   Pure JS object
+ *  @param      {function}  options.propertyHandler     -   Handler method runs after property setting
+ *  @param      {function}  options.computedProperties  -   New property method
+ *
+ * **/
 Publisher.prototype._init = function (options) {
     this._event = options.event || null;
     this._state = options.state || null;
@@ -218,6 +358,13 @@ Publisher.prototype._init = function (options) {
     this.computedPropertiesMethod();
 };
 
+/**
+ *  @summary    Handler method runs after property setting
+ *
+ *  @public
+ *  @function   propertyHandlerMethod
+ *
+ * **/
 Publisher.prototype.propertyHandlerMethod = function () {
     var bundle = {context: this, state: this._state};
 
@@ -230,6 +377,13 @@ Publisher.prototype.propertyHandlerMethod = function () {
     }
 };
 
+/**
+ *  @summary    New property method
+ *
+ *  @public
+ *  @function   computedPropertiesMethod
+ *
+ * **/
 Publisher.prototype.computedPropertiesMethod = function () {
     var bundle = {context: this, state: this._state};
 
@@ -243,6 +397,13 @@ Publisher.prototype.computedPropertiesMethod = function () {
     }
 };
 
+/**
+ *  @summary    Publisher's event name set/get accessor
+ *
+ *  @public
+ *  @function   event
+ *
+ * **/
 Publisher.prototype.event = function () {
     if (arguments.length) {
         this._event = arguments[0];
@@ -251,6 +412,13 @@ Publisher.prototype.event = function () {
     return this._event;
 };
 
+/**
+ *  @summary    Publisher's state set/get accessor
+ *
+ *  @public
+ *  @function   state
+ *
+ * **/
 Publisher.prototype.state = function () {
     if (arguments[0] instanceof Object) {
         this._state = arguments[0];
@@ -259,17 +427,44 @@ Publisher.prototype.state = function () {
     }
 };
 
+/**
+ *  @summary    toString
+ *
+ *  @public
+ *  @override
+ *  @function   toString
+ *
+ *  @return {object}
+ * **/
 Publisher.prototype.toString = function () {
     return "Event : " + this._event + " " + " State : " + this._state;
 };
 
 /**
- * Subscriber
+ *  @constructor
+ *
+ *  @function   Subscriber
+ *
+ *  @param  {object}    options
+ *  @return {object}    Subscriber instance
+ *
  * **/
 function Subscriber(options) {
     this._init(options);
 }
 
+/**
+ *  @summary    private constructor's member
+ *
+ *
+ *  @function   _init
+ *
+ *  @param      {object}    options
+ *  @param      {string}    options.id                  -   Unique subscriber identity
+ *  @param      {string}    options.event               -   Eventbus's event name
+ *  @param      {function}  options.callback            -   Calls after publisher' state set
+ *
+ * **/
 Subscriber.prototype._init = function (options) {
     this._id = options.id || null;
     this._event = options.event || null;
@@ -281,6 +476,13 @@ Subscriber.prototype._init = function (options) {
     }
 };
 
+/**
+ *  @summary    Subsciber's event name set/get accessor
+ *
+ *  @public
+ *  @function   event
+ *
+ * **/
 Subscriber.prototype.event = function () {
     if (arguments.length) {
         this._event = arguments[0];
@@ -289,6 +491,13 @@ Subscriber.prototype.event = function () {
     return this._event;
 };
 
+/**
+ *  @summary    Calls after publisher' state set
+ *
+ *  @public
+ *  @function   callback
+ *
+ * **/
 Subscriber.prototype.callback = function () {
     if (arguments[0] instanceof Function) {
         this._callback = arguments[0];
@@ -297,22 +506,24 @@ Subscriber.prototype.callback = function () {
     }
 };
 
+/**
+ *  @summary    Subsciber's identity set accessor
+ *
+ *  @public
+ *  @function   setId
+ *
+ * **/
 Subscriber.prototype.setId = function (id) {
     this._id = id;
 };
 
+/**
+ *  @summary    Subsciber's identity get accessor
+ *
+ *  @public
+ *  @function   getId
+ *
+ * **/
 Subscriber.prototype.getId = function () {
     return this._id;
-};
-
-/**
- * Utility
- * **/
-
-Eventbus.prototype._isEvent = function (eventName) {
-    if (this._eventbus.hasOwnProperty(eventName)) {
-        return true;
-    }
-
-    throw "Anatolia Error : Unknown event!"
 };
