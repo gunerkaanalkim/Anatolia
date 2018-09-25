5da49bbcbe7674b927f57cd33ead6a872565d022
gunerkaanalkim <g.kaanalkim@gmail.com>
2018-09-25 18:08:40 +0300

    test > Eventbus registers multiple subscribers

--- src/test/framework/state-management/eventbus/Eventbus.test.js
+++ src/test/framework/state-management/eventbus/Eventbus.test.js
@@ -60,8 +60,40 @@ describe('Eventbus', function () {
             });
 
             eventbus.subscriber().register(subscriber);
+        });
+    });
 
+    describe('subscriber()#register-multiple', function () {
+        it('Eventbus registers multiple subscribers', function () {
+            var eventbus = new Eventbus();
 
+            var publisher_1 = new Publisher({
+                event: "event_1",
+                state: {
+                    foo: "bar",
+                    tar: "zar"
+                }
+            });
+
+            eventbus.publisher().register(publisher_1);
+
+            var subscriber_1 = new Subscriber({
+                id: "sub1",
+                event: "event_1",
+                callback: function (state) {
+                    assert.equal(state.event_1.foo, "bar");
+                }
+            });
+
+            var subscriber_2 = new Subscriber({
+                id: "sub2",
+                event: "event_1",
+                callback: function (state) {
+                    assert.equal(state.event_1.tar, "zar");
+                }
+            });
+
+            eventbus.subscriber().register(subscriber_1, subscriber_2);
         });
     });
 });
