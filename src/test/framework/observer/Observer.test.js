6c91678fa4d173c606ca09e22a94079c4148a1d1
gunerkaanalkim <g.kaanalkim@gmail.com>
2018-09-25 18:53:25 +0300

    test > object watching

---
+++ src/test/framework/observer/Observer.test.js
@@ -0,0 +1,21 @@
+var assert = require("assert");
+var Observer = require("../../../../src/js/framework/observer/Observer");
+
+describe('Observer', function () {
+    describe('Object watching', function () {
+        it('Handler method gives key, old value and new value.', function () {
+            var obj = {
+                name: "Güner ALKIM",
+                email: "g.kaanalkim@gmail.com"
+            };
+
+            Observer.watch(obj, function (key, oldValue, newValue) {
+                assert.equal(key, "name");
+                assert.equal(oldValue, "Güner ALKIM");
+                assert.equal(newValue, "Güner Kaan ALKIM");
+            });
+
+            obj.name = "Güner Kaan ALKIM";
+        });
+    });
+});
