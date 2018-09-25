74abf14dc30908253fe758e4ae448a1f2f50dfd8
gunerkaanalkim <g.kaanalkim@gmail.com>
2018-09-25 18:18:24 +0300

    test > Eventbus publisher()'s fire method trigger callback of subscriber of publisher.

--- src/test/framework/state-management/eventbus/Eventbus.test.js
+++ src/test/framework/state-management/eventbus/Eventbus.test.js
@@ -96,4 +96,40 @@ describe('Eventbus', function () {
             eventbus.subscriber().register(subscriber_1, subscriber_2);
         });
     });
+
+    describe('publisher()#fire', function () {
+        it('Eventbus publisher()\'s fire method trigger callback of subscriber of publisher.', function () {
+            var eventbus = new Eventbus();
+
+            var publisher_1 = new Publisher({
+                event: "event_1",
+                state: {
+                    foo: "bar",
+                    tar: "zar"
+                }
+            });
+
+            eventbus.publisher().register(publisher_1);
+
+            var subscriber_1 = new Subscriber({
+                id: "sub1",
+                event: "event_1",
+                callback: function (state) {
+                    assert.equal(state.event_1.foo, "bar");
+                }
+            });
+
+            var subscriber_2 = new Subscriber({
+                id: "sub2",
+                event: "event_1",
+                callback: function (state) {
+                    assert.equal(state.event_1.tar, "zar");
+                }
+            });
+
+            eventbus.subscriber().register(subscriber_1, subscriber_2);
+            eventbus.publisher().fire(publisher_1);
+
+        });
+    });
 });
