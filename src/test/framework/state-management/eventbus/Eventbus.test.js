19fd1f818fb84aecb3f51df8c477811d9b14bb63
gunerkaanalkim <g.kaanalkim@gmail.com>
2018-09-25 17:59:29 +0300

    test > Eventbus registers multiple publishers

--- src/test/framework/state-management/eventbus/Eventbus.test.js
+++ src/test/framework/state-management/eventbus/Eventbus.test.js
@@ -4,7 +4,7 @@ var Subscriber = require("../../../../js/framework/state-management/subscriber/S
 var Publisher = require("../../../../js/framework/state-management/publisher/Publisher");
 
 describe('Eventbus', function () {
-    describe('publisher()#register', function () {
+    describe('publisher()#register-single', function () {
         it('Eventbus registers a publisher', function () {
             var eventbus = new Eventbus();
 
@@ -16,6 +16,27 @@ describe('Eventbus', function () {
             eventbus.publisher().register(publisher);
 
             assert.equal(eventbus.getSubscriptionList()[0], "event_1");
+        });
+    });
+
+    describe('publisher()#register-multiple', function () {
+        it('Eventbus registers multiple publishers', function () {
+            var eventbus = new Eventbus();
+
+            var publisher_1 = new Publisher({
+                event: "event_1",
+                state: {foo: "bar"}
+            });
+
+            var publisher_2 = new Publisher({
+                event: "event_2",
+                state: {foo: "bar"}
+            });
+
+            eventbus.publisher().register(publisher_1, publisher_2);
+
+            assert.equal(eventbus.getSubscriptionList()[0], "event_1");
+            assert.equal(eventbus.getSubscriptionList()[1], "event_2");
         });
     });
 });
