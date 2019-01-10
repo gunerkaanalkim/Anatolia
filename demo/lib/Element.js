aa72b3fcaa1f62dffd8877ca18fec77122755976
gunerkaanalkim <g.kaanalkim@gmail.com>
2019-01-10 10:22:56 +0300

    memoize function wrapper implementation

--- demo/lib/Element.js
+++ demo/lib/Element.js
@@ -1,79 +1,95 @@
-function benchmark() {
-    var suite = new Benchmark.Suite;
+// function benchmark() {
+//     var suite = new Benchmark.Suite;
+//
+//     suite
+//         .add('Anatolia_1', function () {
+//             var state1 = {
+//                 value: Math.random()
+//             };
+//
+//             new MemoizeComponent({
+//                 container: "#component_1",
+//                 state: state1,
+//                 render: function (state) {
+//                     var button = this.element.create("button1", "a", {
+//                             class: "btn btn-primary",
+//                             text: this.state.value
+//                         }
+//                     );
+//
+//                     return button;
+//                 }
+//             }).render();
+//         })
+//         .add('Anatolia_2', function () {
+//             var state2 = {
+//                 value: Math.random()
+//             };
+//
+//             new Component({
+//                 container: "#component_2",
+//                 state: state2,
+//                 render: function (state) {
+//                     var button = Component.createElement("a", {
+//                         class: "btn btn-warning",
+//                         text: state.value
+//                     });
+//
+//                     return button;
+//                 }
+//             }).render();
+//         })
+//         .add('Vue', function () {
+//             var value = Math.random();
+//
+//             Vue.component('my_component', {
+//                 data: function () {
+//                     return {
+//                         value: value
+//                     }
+//                 },
+//                 template: '<a class="btn btn-default">{{value}}</a>'
+//             });
+//
+//             new Vue({el: "#component_3"})
+//         })
+//         .add('React', function () {
+//             class Sample extends React.Component {
+//                 render() {
+//                     return React.createElement("a", null, Math.random())
+//                 }
+//             }
+//
+//             ReactDOM.render(
+//                 React.createElement(Sample, null, null),
+//                 document.getElementById('component_4')
+//             );
+//         })
+//         // add listeners
+//         .on('cycle', function (event) {
+//             console.log(String(event.target));
+//         })
+//         .on('complete', function () {
+//             console.log('Fastest is ' + this.filter('fastest').map('name'));
+//         })
+//         // run async
+//         .run({'async': true});
+// }
+//
+// benchmark();
 
-    suite
-        .add('Anatolia_1', function () {
-            var state1 = {
-                value: Math.random()
-            };
-
-            new MemoizeComponent({
-                container: "#component_1",
-                state: state1,
-                render: function (state) {
-                    var button = this.element.create("button1", "a", {
-                            class: "btn btn-primary",
-                            text: this.state.value
-                        }
-                    );
-
-                    return button;
-                }
-            }).render();
-        })
-        .add('Anatolia_2', function () {
-            var state2 = {
-                value: Math.random()
-            };
-
-            new Component({
-                container: "#component_2",
-                state: state2,
-                render: function (state) {
-                    var button = Component.createElement("a", {
-                        class: "btn btn-warning",
-                        text: state.value
-                    });
-
-                    return button;
-                }
-            }).render();
-        })
-        .add('Vue', function () {
-            var value = Math.random();
-
-            Vue.component('my_component', {
-                data: function () {
-                    return {
-                        value: value
-                    }
-                },
-                template: '<a class="btn btn-default">{{value}}</a>'
-            });
-
-            new Vue({el: "#component_3"})
-        })
-        .add('React', function () {
-            class Sample extends React.Component {
-                render() {
-                    return React.createElement("a", null, Math.random())
-                }
-            }
-
-            ReactDOM.render(
-                React.createElement(Sample, null, null),
-                document.getElementById('component_4')
-            );
-        })
-        // add listeners
-        .on('cycle', function (event) {
-            console.log(String(event.target));
-        })
-        .on('complete', function () {
-            console.log('Fastest is ' + this.filter('fastest').map('name'));
-        })
-        // run async
-        .run({'async': true});
+function sum(a, b) {
+    return a + b;
 }
 
-benchmark();
+var memoizeSum = Memoization(sum);
+console.log(memoizeSum(2, 3));
+
+
+function sum2(options) {
+    return options.val1 + options.val2;
+}
+
+var memoizedSum2 = Memoization(sum2);
+console.log(memoizedSum2({val1: 3, val2: 4}));
+console.log(memoizedSum2({val1: 5, val2: 8}));
--- src/js/framework/memoization/Memoization.js
+++ src/js/framework/memoization/Memoization.js
@@ -1,3 +1,62 @@
-function Memoization() {
-
+function Memoization(fn) {
+    return Memoization.arityControl({
+        method: fn,
+        cache: new MemoizationCache()
+    });
 }
+
+Memoization.arityControl = function (options) {
+    var methodByArity = options.method.length === 1 ? Memoization.monadic : Memoization.variadic;
+
+    return methodByArity.bind(this, options.method, options.cache);
+};
+
+Memoization.monadic = function (fn, cache, args) {
+    var cacheKey = Memoization.serializer(args);
+
+    var computedValue;
+    if (!cache.has(cacheKey)) {
+        computedValue = fn.call(this, args);
+        cache.set(cacheKey, computedValue)
+    }
+
+    return computedValue;
+};
+
+Memoization.variadic = function (fn, cache) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    var cacheKey = Memoization.serializer(args);
+
+    var computedValue;
+    if (!cache.has(cacheKey)) {
+        computedValue = fn.apply(this, args);
+        cache.set(cacheKey, computedValue)
+    }
+
+    return computedValue;
+};
+
+Memoization.serializer = function () {
+    return JSON.stringify(arguments);
+};
+
+function MemoizationCache() {
+    this.cache = Object.create(null)
+}
+
+MemoizationCache.prototype.has = function (key) {
+    console.log(this.cache);
+    return (key in this.cache)
+};
+
+MemoizationCache.prototype.get = function (key) {
+    return this.cache[key]
+};
+
+MemoizationCache.prototype.set = function (key, value) {
+    this.cache[key] = value
+};
+
+MemoizationCache.prototype.getCache = function () {
+    return this.cache;
+};
